62fed13324cf802436226b96b071c8a4dea89bdf
Paul Wright <31939113+plwt@users.noreply.github.com>
2024-05-09 22:19:04 +0100

    Completed

--- README.md
+++ README.md
@@ -1,22 +1,2 @@
 # PicoTempLED
 
-Trying to figure out the trigger temperature:
-
-CPU throttles at 85C, the LAN at 70C
-
-So safer option is the 70C internal temp
-
-Pis tend to run 20C to 30C above room temp.
-
-So the safest operating temperature is 40C
-
-Compare room Pico against Pi internal temp
-
-Pi temp = Pico temp for a scale, up the Pico temp to the figure for a Pi temp of 65.
-
-Set light to blink when temperature of Pi triggers fan.
-
-
-https://www.tomshardware.com/how-to/monitor-temperature-raspberry-pi-pico
-
-https://github.com/plwt/TempSense/blob/main/main.py
--- main.py
+++ main.py
@@ -1,86 +1,18 @@
 import machine
 import utime
-from machine import Timer
 
-## Temperature sensor
+led = machine.Pin(25, machine.Pin.OUT)
+
 sensor_temp = machine.ADC(machine.ADC.CORE_TEMP)
-
-## Convert to Celsius
 conversion_factor = 3.3 / (65535)
+reading = sensor_temp.read_u16() * conversion_factor
+temperature = 27 - (reading - 0.706)/0.001721
 
-
-## Setup the onboard LED
-led_onboard = machine.Pin(25, machine.Pin.OUT)
-
-## Setup the onboard LED for off
-def leds_off():
-    """Turn off all the LEDs."""
-    led_onboard.value(0)
-
-## Setup the onboard LED for on
-def leds_on():
-    """Turn on all the LEDs."""
-    led_onboard.value(1)
-
-## Read the temperature, write to file, and turn on the LED
-while True:
-    reading = sensor_temp.read_u16() * conversion_factor
-    temperature = 27 - (reading - 0.706)/0.001721
-    
-    ## Minimum temperature
-    if temperature <= 12.00:
-        leds_off()
-        led_onboard.value(1)
-    
-    ## Maximum temperature
-    elif temperature > 28.00:
-        leds_off()
-        led_onboard.value(1)
-    
-    ## Outside of the range, turn off the LED
-    else:
-        leds_off()
-
-
-
-
-
-
-
-python
-import machine
-import time
-import dht
-
-# Configure the temperature sensor
-sensor = dht.DHT11(machine.Pin(4))  # Assuming the sensor is connected to GPIO 4
-
-# Configure the LED
-led = machine.Pin(2, machine.Pin.OUT)  # Assuming the LED is connected to GPIO 2
-
-# Set the temperature threshold (in Celsius)
-temperature_threshold = 30
 
 while True:
-    try:
-        sensor.measure()  # Trigger a temperature measurement
-        temperature = sensor.temperature()  # Get the temperature in Celsius
-
-        if temperature > temperature_threshold:
-            # Blink the LED if temperature exceeds the threshold
-            led.value(1)  # Turn on the LED
-            time.sleep(0.5)  # Wait for 0.5 seconds
-            led.value(0)  # Turn off the LED
-            time.sleep(0.5)  # Wait for 0.5 seconds
-        else:
-            led.value(0)  # Keep the LED off
-
-        print("Temperature: {:.1f} °C".format(temperature))
-
-    except OSError as e:
-        print("Error: {}".format(e))
-
-    time.sleep(2)  # Wait for 2 seconds before taking the next measurement
-
-
+    if temperature > 30:
+        led.value(1)
+        utime.sleep(0.5)
+        led.value(0)
+        utime.sleep(3)
 
